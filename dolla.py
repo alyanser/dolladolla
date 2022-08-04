bec1aea92c9ea7d1da3873feb9e050c5441174e5
Aly Anser <alyystaz@gmail.com>
2022-08-05 00:59:16 +0500

    made ig to work

--- dolla.py
+++ dolla.py
@@ -10,12 +10,27 @@ def click_on_earn_pages_button():
 
 		try:
 			driver.find_element(By.CSS_SELECTOR, '.earn_pages_button').click()
+			time.sleep(3)
+			driver.switch_to.window(driver.window_handles[1])
 			return True
 		except:
 			driver.refresh()
 
 	driver.back()
 	return False
+
+def click_on_confirm_button():
+
+	for _ in range(5):
+
+		try:
+			driver.find_element(By.CSS_SELECTOR, 'img[title="Click On The Button To Confirm Interaction!"]').click()
+			print("Action completed")
+			return
+		except:
+			pass
+
+	print("could not clickon confirm button")
 
 def twitter_follows():
 	print("twitter follows in process...")
@@ -25,14 +40,6 @@ def twitter_follows():
 
 		if not click_on_earn_pages_button():
 			return
-
-		try:
-
-			driver.switch_to.window(driver.window_handles[1])
-		except:
-			print("something went wrong. new window wasn't opened")
-			driver.refresh()
-			continue
 
 		try:
 			twit_user_inp = driver.find_element(By.NAME, "session[username_or_email]")
@@ -47,33 +54,19 @@ def twitter_follows():
 			# already logged in
 			pass
 
-		time.sleep(2)
+		time.sleep(3)
 		actions.send_keys(Keys.RETURN)
 		actions.perform()
 		time.sleep(2)
 		driver.close()
 		driver.switch_to.window(driver.window_handles[0])
 
-		driver.find_element(By.CSS_SELECTOR, 'img[title="Click On The Button To Confirm Interaction!"]').click()
-		print("Action completed")
-
-def twitter_retweets():
-	print("twitter retweets in process...")
-	driver.find_element(By.CSS_SELECTOR, 'a[title="Earn Credits By Twitter Retweet"]').click()
-
-	while True:
-
-		if not click_on_earn_pages_button():
-			return
-
-def insta_likes():
-	print("instagram likes in process...")
+		click_on_confirm_button()
 
 def insta_follows():
 	print("instagram follows in process...")
 	actions.send_keys(Keys.DOWN * 8)
 	actions.perform()
-	time.sleep(3)
 	driver.find_element(By.CSS_SELECTOR, 'a[title="Earn Credits By Instagram Follows"]').click()
 
 	while True:
@@ -81,18 +74,14 @@ def insta_follows():
 		if not click_on_earn_pages_button():
 			return
 
-		try:
-			username = driver.find_element(By.NAME, "name[username]")
-			password = driver.find_element(By.NAME, "name[password]")
+		actions.send_keys(Keys.TAB * 3)
+		actions.send_keys(Keys.RETURN)
+		actions.perform()
 
-			username.send_keys(insta_username)
-			password.send_keys(insta_password)
-			password.send_keys(Keys.RETURN)
-		except:
-			pass
-
-def twitter_likes():
-	print("twitter likes in process...")
+		time.sleep(3)
+		driver.close()
+		driver.switch_to.window(driver.window_handles[0])
+		click_on_confirm_button()
 
 like_username = "testingsecondacc"
 like_password = "123123.Tt"
@@ -110,7 +99,7 @@ if __name__ == '__main__':
 	options.add_argument("--headless")
 
 	driver = webdriver.Firefox(options = options)
-	driver.implicitly_wait(50)
+	driver.implicitly_wait(15)
 	actions = ActionChains(driver)
 
 	print("loading the webpage...")
@@ -137,10 +126,10 @@ if __name__ == '__main__':
 	while True:
 
 		try:
-			twitter_follows()
-			# insta_follows()
+			# twitter_follows()
+			insta_follows()
 		except KeyboardInterrupt:
 			exit(1)
 		except:
-			driver.close()
+			driver.quit()
 			raise
