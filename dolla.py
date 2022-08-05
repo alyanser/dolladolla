472c8ce41cdd2aae21bca9ac72b40acccb1e452a
Aly Anser <alyystaz@gmail.com>
2022-08-05 15:08:52 +0500

    added firefox profile + fixed scrolling issue

--- dolla.py
+++ dolla.py
@@ -1,4 +1,6 @@
 import time
+from os.path import exists
+from os import mkdir
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -6,7 +8,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 def click_on_earn_pages_button():
 
-	for _ in range(5):
+	for _ in range(3):
 
 		try:
 			driver.find_element(By.CSS_SELECTOR, '.earn_pages_button').click()
@@ -30,10 +32,13 @@ def click_on_confirm_button():
 		except:
 			pass
 
-	print("could not clickon confirm button")
+	print("could not click on confirm button")
 
 def twitter_follows():
 	print("twitter follows in process...")
+	actions.send_keys(Keys.UP * 8)
+	actions.perform()
+	time.sleep(2)
 	driver.find_element(By.CSS_SELECTOR, 'a[title="Earn Credits By Twitter Follows"]').click()
 
 	while True:
@@ -57,7 +62,7 @@ def twitter_follows():
 		time.sleep(3)
 		actions.send_keys(Keys.RETURN)
 		actions.perform()
-		time.sleep(2)
+		time.sleep(3)
 		driver.close()
 		driver.switch_to.window(driver.window_handles[0])
 
@@ -67,6 +72,7 @@ def insta_follows():
 	print("instagram follows in process...")
 	actions.send_keys(Keys.DOWN * 8)
 	actions.perform()
+	time.sleep(2)
 	driver.find_element(By.CSS_SELECTOR, 'a[title="Earn Credits By Instagram Follows"]').click()
 
 	while True:
@@ -74,14 +80,19 @@ def insta_follows():
 		if not click_on_earn_pages_button():
 			return
 
+		time.sleep(4)
+
+		# it's buggy. use CSS_SELECTOR
 		actions.send_keys(Keys.TAB * 3)
 		actions.send_keys(Keys.RETURN)
 		actions.perform()
 
-		time.sleep(3)
+		time.sleep(4)
 		driver.close()
 		driver.switch_to.window(driver.window_handles[0])
 		click_on_confirm_button()
+
+profile_dir = "firefox-profile"
 
 like_username = "testingsecondacc"
 like_password = "123123.Tt"
@@ -95,38 +106,47 @@ insta_password = "reallysomeone123"
 if __name__ == '__main__':
 	print("dolla dolla bill y'all")
 	print("starting the bot...")
+
+	if not exists(profile_dir):
+		mkdir(profile_dir)
+
 	options = webdriver.FirefoxOptions()
+
 	options.add_argument("--headless")
+	options.add_argument("--profile") 
+	options.add_argument(profile_dir) 
 
 	driver = webdriver.Firefox(options = options)
-	driver.implicitly_wait(15)
+
+	driver.implicitly_wait(30)
 	actions = ActionChains(driver)
 
-	print("loading the webpage...")
+	driver.get(like_url)
 
 	try:
-		driver.get(like_url)
+		username = driver.find_element(By.ID, "username")
+		password = driver.find_element(By.ID, "password")
+
+		username.clear()
+		password.clear()
+
+		username.send_keys(like_username)
+		password.send_keys(like_password)
+		password.send_keys(Keys.RETURN)
 	except:
-		print("could not load the webpage. check your connection and try again")
+		pass
+
+	try:
+		driver.find_element(By.CSS_SELECTOR, 'a[title="Earn Credits"]').click()
+		print("login successful")
+	except:
+		print("could not login. exiting...")
 		exit(1)
-
-	username = driver.find_element(By.ID, "username")
-	password = driver.find_element(By.ID, "password")
-
-	username.clear()
-	password.clear()
-
-	username.send_keys(like_username)
-	password.send_keys(like_password)
-	password.send_keys(Keys.RETURN)
-
-	driver.find_element(By.CSS_SELECTOR, 'a[title="Earn Credits"]').click()
-	print("login sucessful")
 
 	while True:
 
 		try:
-			# twitter_follows()
+			twitter_follows()
 			insta_follows()
 		except KeyboardInterrupt:
 			exit(1)
